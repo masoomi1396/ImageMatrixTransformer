8824e845e5c8b49ee6d889c83b7536de2abeba4f
Amirmohammad Masoumi <39368982+masoomi1396@users.noreply.github.com>
2026-04-18 13:12:47 +0200

    Add image rotation function with bilinear interpolation

--- Image.py
+++ Image.py
@@ -67,9 +67,101 @@ def scalingImage(image, factor):
     newImage.show()
             
 
-      
+def findingBoundryOfRotation(width,height,angle,point):
+    sinAngle = math.sin(angle)
+    cosAngle = math.cos(angle)
+    
+    x1 = 0
+    x2 = width - 1
+    y1 = 0
+    y2 = height - 1
+    
+    xNew = []
+    yNew = []
+    corners = [(x1,y1),(x1,y2),(x2,y1),(x2,y2)]
+    
+    for corner in corners:
+        x = corner[0]
+        y = corner[1]
+        xShifted = x - point[0]
+        yShifted = y - point[1]
+        xRot =  xShifted * cosAngle - yShifted * sinAngle
+        yRot =  xShifted * sinAngle + yShifted * cosAngle
+        xNew.append(xRot + point[0]) 
+        yNew.append(yRot + point[1]) 
+    xNew.sort()
+    yNew.sort()
+    
+    newWidth = xNew[3] - xNew[0] 
+    newHeight = yNew[3] - yNew[0]
+    
+    offsetX = -xNew[0]
+    offsetY = -yNew[0]
+    
+    return math.ceil(newWidth), math.ceil(newHeight), offsetX, offsetY
+    
+    pass
+
+
+def rotatedImage(image, angle, point=(0,0)):
+    angle *= math.pi / 180
+    width, height = image.size
+    rotationX = point[0]
+    rotationY = point[1]
+    sinAngle = math.sin(angle)
+    cosAngle = math.cos(angle)
+    R,G,B = imageMatrixConversion(image)
+    
+    newWidth, newHeight, offsetX, offsetY = findingBoundryOfRotation(width, height, angle, point)
+    newR = [[0 for _ in range(newWidth)] for _ in range(newHeight)]
+    newG = [[0 for _ in range(newWidth)] for _ in range(newHeight)]
+    newB = [[0 for _ in range(newWidth)] for _ in range(newHeight)]
+    
+    
+    
+    for y in range(newHeight):
+        yShifted = y - offsetY
+        yCentered = yShifted - rotationY
+        for x in range(newWidth):
+            xShifted = x - offsetX
+            xCentered = xShifted - rotationX
             
+            x_old = xCentered * cosAngle + yCentered * sinAngle
+            y_old = (-xCentered) * sinAngle + yCentered * cosAngle
+            
+            x_src = x_old + rotationX
+            y_src = y_old + rotationY
+            
+            if x_src < 0 or x_src >= width or y_src < 0 or y_src >= height:
+                newR[y][x],newG[y][x],newB[y][x] = 0,0,0
+                continue
+            
+            y1 = math.floor(y_src)
+            y2 = min(y1 + 1, height - 1)
+            dY = y_src - y1
+            
+            x1 = math.floor(x_src)
+            x2 = min(x1 + 1, width - 1)
+            dX = x_src - x1
+            
+            red_00, red_01, red_10, red_11 = R[y1][x1], R[y1][x2], R[y2][x1], R[y2][x2]
+            green_00, green_01, green_10, green_11 = G[y1][x1], G[y1][x2], G[y2][x1], G[y2][x2]
+            blue_00,blue_01,blue_10,blue_11 = B[y1][x1], B[y1][x2], B[y2][x1], B[y2][x2]
+            
+            w_00 = (1 - dX) * (1 - dY)
+            w_01 = (dX) * (1 - dY)
+            w_10 = (1 - dX) * (dY)
+            w_11 = (dX) * (dY)
+            
+            red = ( red_00 * w_00 + red_01 * w_01 + red_10 * w_10 + red_11 * w_11 )
+            green = ( green_00 * w_00 + green_01 * w_01 + green_10 * w_10 + green_11 * w_11 )
+            blue = (blue_00 * w_00 + blue_01 * w_01 + blue_10 * w_10 + blue_11 * w_11)
+            
+            newR[y][x],newG[y][x],newB[y][x] = int(min(255,max(red,0))),int(min(255,max(green,0))),int(min(255,max(blue,0)))
+            
+    newImage = matrixImageConversion(newR, newG, newB)
+    newImage.show()
     
 img = Image.open("Test.png")
 
-scalingImage(img, 3)
+rotatedImage(img, 90, (500,100))
